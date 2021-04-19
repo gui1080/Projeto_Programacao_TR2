095bd6c1e40f4a59e1f2dd98445c35e537aec058
Guilherme B <38567020+gui1080@users.noreply.github.com>
2021-04-19 11:34:32 -0700

    calculando delta

--- Codigo/pydash/r2a/r2atrabalhotr2.py
+++ Codigo/pydash/r2a/r2atrabalhotr2.py
@@ -7,6 +7,7 @@
 from r2a.ir2a import IR2A
 from player.parser import *
 import time
+import math
 from statistics import mean
 
 
@@ -27,6 +28,13 @@ class R2ATrabalhoTR2(IR2A):
         self.request_time = 0               # tempo de solicitação
         self.passagem = 0                   # quantidade de vezes que passou pelo código
         self.ts_menos1 = 0                  # Ts[i-1]
+        self.delta = 0                      # delta, para a fórmula de Te
+        self.te_menos2 = 0                  # Te[i-2]
+
+        self.P0 = 0.05                      # parametros arbitrarios para calculo de delta 
+        self.k = 21                         # relacionados com a qualidade da conexão
+        
+        self.p = 0                          # desvio padrão normalizado sendo calculado
 
     def handle_xml_request(self, msg):
 
@@ -47,10 +55,11 @@ class R2ATrabalhoTR2(IR2A):
 
         self.request_time = time.perf_counter()      # inicia a contagem
 
+        #----------------------------------------
         # calcular o delta e o desvio!
 
         # k =  21       #parâmetro k da função logística delta
-        # P0 = 0.2      #parâmetro P0 da função logística delta
+        # P0 = 0.5      #parâmetro P0 da função logística delta
 
         #p = |throughputs[0] - estimados_throughputs[i]| / estimados_throughputs[i]             # desvio normalizado de vazão
 
@@ -63,23 +72,54 @@ class R2ATrabalhoTR2(IR2A):
 
         # para obter a restrição de taxa de bits Rc(i) a partir da taxa de transferência estimada,uma média de segurança mi é usada
         #Rc[i] = (1-mi)*estimados_throughputs[i]        # restrição de taxa de bits com mi[0, 0.5]
+        #----------------------------------------
 
-        # nas primeiras duas passagens não tem vazão para estimar
-        # então enche com as vazões medidas no começo
+        # PARÂMETROS PARA ESTIMAR A VAZÃO
+
+        # Nas primeiras duas passagens não tem vazão para estimar
+        # Então enche com as vazões medidas no começo
         if (self.passagem == 0):
             self.estimados_throughputs.append(self.throughputs[0])
+            vazao_atual = self.throughputs[0]               # salvamos esse valor para calcular o desvio
+            self.delta = 0                                  # primeira vez que algoritmo roda, não tem Te(0) para calcular delta, nenhuma variação ocorreu
+
         if (self.passagem == 1):
-            self.estimados_throughputs.append(self.throughputs[1])
-        # quando for tirar o mais antigo dessa lista, vai sair Te(i-2)
+            self.estimados_throughputs.append(self.throughputs[1])          # quando for tirar o mais antigo dessa lista, vai sair Te(i-2)
+        
 
         if (self.passagem > 0):
-            # pegando Ts(i-1)
-            self.ts_menos1 = self.throughputs[0]
-            # mantendo uma lista só com throughput atual, próxima iteração vira Ts(i-1)
-            del self.throughputs[0]
+            self.ts_menos1 = self.throughputs[0]            # pegando Ts(i-1)
+            del self.throughputs[0]                         # mantendo uma lista só com throughput atual, próxima iteração vira Ts(i-1)
+
+        #----------------------------------------
+
+        # CALCULO DO DELTA
+
+        # math.exp(x) retorna e^x
+        # k e P0 dependem da conexão 
+        self.delta = ( 1 / (1 + (math.exp(-self.k*(self.p - self.P0)) ) ) )
+
+        #----------------------------------------
 
         # calcula Te(i), bota ele na lista de vazões estimadas, deleta o mais antigo, passa para frente
 
+        if (self.passagem > 1):
+            
+            # no final, temos que colocar o novo Te(i) na fila, para o ciclo repetir
+            
+            self.te_menos2 = self.estimados_throughputs[0]                  # podemos retirar o elemento mais antigo dessa fila, sendo o Te(i-2)
+
+            estimativa_atual = (( 1 - self.delta ) * self.te_menos2 ) + ( self.delta * self.ts_menos1 )
+
+            
+            self.estimados_throughputs.append(estimativa_atual)             # damos append na nova estimativa e deletamos
+            del self.estimados_throughputs[0] 
+
+        else:
+            estimativa_atual = 0                                            # começa com zero
+
+
+        # Prints :)
         print("-----VAZÃO-------")
         print(self.throughputs)
         print("------------")
@@ -89,6 +129,20 @@ class R2ATrabalhoTR2(IR2A):
         print("------ LISTA DE VAZÃO ESTIMADA ------")
         print(self.estimados_throughputs)
         print("------------")
+        print("------ DELTA ------")
+        print(self.delta)
+        print("------------")
+        print("------ ESTIMATIVA ATUAL ------")
+        print(estimativa_atual)
+        print("------------")
+
+
+        # CALCULO DO DESVIO 
+        # calculamos o desvio pra próxima iteração saber o valor necessário de p
+
+        self.p = 0.40           # placeholder pra equação do delta rodar
+
+        #----------------------------------------
 
         # seleciona qualidades para envio
         # qi[0] = 46980bps
